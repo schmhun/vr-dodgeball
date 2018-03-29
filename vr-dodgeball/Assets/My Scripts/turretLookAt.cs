8c961b577786b8241e997dd5db2a50ba8066a843
Hunter Schmitt <schmhun@users.noreply.github.com>
2018-03-28 23:59:33 -0500

    Garrett's Prefabs
    Co-Authored-By: Garrett Beebe <gb82366@users.noreply.github.com>

---
+++ vr-dodgeball/Assets/My Scripts/turretLookAt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretLookAt : MonoBehaviour {
+    public GameObject target;
+    public float turn_speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateTowards(target.transform.position);
+	}
+    protected void rotateTowards(Vector3 to)
+    {
+
+        Quaternion _lookRotation = Quaternion.LookRotation((to - transform.position).normalized);
+
+        //over time
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * turn_speed);
+
+    }
+}
